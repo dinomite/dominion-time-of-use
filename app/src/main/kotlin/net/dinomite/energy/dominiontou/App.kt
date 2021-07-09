2ae83c8d19f187e84a752cc4f3862a8d3071620f
Drew Stephens <drew@dinomite.net>
2021-07-09 14:19:44 -0400

    Model pricing

--- app/src/main/kotlin/net/dinomite/energy/dominiontou/App.kt
+++ app/src/main/kotlin/net/dinomite/energy/dominiontou/App.kt
@@ -1,8 +1,73 @@
 package net.dinomite.energy.dominiontou
 
+import net.dinomite.energy.dominiontou.Season.SUMMER
+import net.dinomite.energy.dominiontou.Season.WINTER
+import net.dinomite.energy.dominiontou.SeasonalPrice.PriceGroup
+import net.dinomite.energy.dominiontou.SeasonalPrice.PriceGroup.Period
+import net.dinomite.energy.dominiontou.SeasonalPrice.PriceGroup.PricePeriod
 import java.io.File
+import java.math.BigDecimal
 import java.time.Instant
+import java.time.LocalTime
 import java.time.ZoneId
+
+enum class Season(val months: Set<Int>) {
+    SUMMER(listOf(5..9).flatten().toSet()),
+    WINTER((listOf(1..4) + listOf(10..12)).flatten().toSet());
+}
+
+class SeasonalPrice(
+    val weekday: PriceGroup,
+    val weekend: PriceGroup
+) {
+    // TODO Ensure all days are covered
+    class PriceGroup(
+        val superOffPeak: PricePeriod,
+        val offPeak: PricePeriod,
+        val onPeak: PricePeriod? = null
+    ) {
+        // TODO Ensure all periods covered
+        class Period(
+            val start: LocalTime,
+            val end: LocalTime
+        ) {
+            constructor(startHour: Int, endHour: Int) :
+                    this(LocalTime.of(startHour, 0), LocalTime.of(endHour, 0))
+        }
+
+        class PricePeriod(
+            val periods: List<Period>,
+            val price: BigDecimal
+        ) {
+            constructor(period: Period, price: BigDecimal) : this(listOf(period), price)
+        }
+    }
+}
+
+val pricing = mapOf(
+    SUMMER to SeasonalPrice(
+        weekday = PriceGroup(
+            superOffPeak = PricePeriod(listOf(Period(0, 5)), BigDecimal(0.077139)),
+            offPeak = PricePeriod(listOf(Period(5, 15), Period(18, 24)), BigDecimal(0.095826)),
+            onPeak = PricePeriod(Period(15, 18), BigDecimal(0.229038))
+        ),
+        weekend = PriceGroup(
+            superOffPeak = PricePeriod(listOf(Period(0, 5)), BigDecimal(0.073712)),
+            offPeak = PricePeriod(Period(5, 24), BigDecimal(0.092399)),
+        )
+    ),
+    WINTER to SeasonalPrice(
+        weekday = PriceGroup(
+            superOffPeak = PricePeriod(listOf(Period(0, 5)), BigDecimal(0.099736)),
+            offPeak = PricePeriod(listOf(Period(5, 6), Period(9, 17), Period(20, 24)), BigDecimal(0.103199)),
+            onPeak = PricePeriod(listOf(Period(6, 9), Period(17, 20)), BigDecimal(0.174449))
+        ),
+        weekend = PriceGroup(
+            superOffPeak = PricePeriod(listOf(Period(0, 5)), BigDecimal(0.099772)),
+            offPeak = PricePeriod(Period(5, 24), BigDecimal(0.096309)),
+        )
+    ),
+)
 
 fun main() {
     // Energy measure at time; strip header row, split to (timestamp, energy)
@@ -10,7 +75,7 @@ fun main() {
         .drop(1)
         .map { it.split(",") }
 
-    var priorEnergy = energyMeasurements[0][1].toDouble()
+    var priorEnergy = energyMeasurements[0][1].toBigDecimal()
     val kwhEachHour = energyMeasurements
         .mapNotNull { (timestamp, energy) ->
             val dateTime = Instant.ofEpochSecond(timestamp.toLong())
@@ -18,13 +83,15 @@ fun main() {
                 .toLocalDateTime()
 
             if (energy != "null") {
-                val thisPeriodEnergy = (energy.toDouble() - priorEnergy)
-                priorEnergy = energy.toDouble()
+                val thisPeriodEnergy = (energy.toBigDecimal() - priorEnergy)
+                priorEnergy = energy.toBigDecimal()
                 dateTime to thisPeriodEnergy
             } else {
                 null
             }
         }
 
-    println(kwhEachHour.last())
+    kwhEachHour
+        .takeLast(10)
+        .forEach { println(it) }
 }
